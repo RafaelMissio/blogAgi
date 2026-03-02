445552941ca1a0df594dc5b452058d382879387c
Rafael Missio <rtmissio@gmail.com>
2026-03-02 20:23:19 -0300

    Adicionando classe BaseTest

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,11 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
+    testImplementation("org.seleniumhq.selenium:selenium-java:4.18.1")
+    implementation("io.github.bonigarcia:webdrivermanager:6.3.3")
+    testImplementation("com.codeborne:selenide:7.6.0")
+    testImplementation("org.slf4j:slf4j-simple:2.0.16")
+
 }
 
 tasks.test {
---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,33 @@
+package base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeEach
+    public void setup() {
+        // Gerencia automaticamente a versão do ChromeDriver
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        options.addArguments("--disable-notifications");
+
+        driver = new ChromeDriver(options);
+        driver.get("https://blog.agibank.com.br/");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
